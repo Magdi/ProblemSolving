f62f722d097493ba9649d9f78ca59e705150a89a
Magdi <acm.magdi@gmail.com>
2021-07-30 10:10:07 +0200

    add MoveZeros solution

---
+++ src/main/kotlin/leetcode/MoveZeros.kt
@@ -0,0 +1,24 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/move-zeroes/
+ *
+ * Given an integer array nums, move all 0's to the end of it while
+ * maintaining the relative order of the non-zero elements.
+    Note that you must do this in-place without making a copy of the array.
+ */
+class MoveZeros {
+    fun moveZeroes(nums: IntArray): Unit {
+        var zeroCount = 0
+        nums.forEachIndexed { index, num ->
+            if (num == 0) {
+                zeroCount++
+            } else {
+                nums[index - zeroCount] = nums[index]
+                if (zeroCount > 0) {
+                    nums[index] = 0
+                }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/leetcode/MoveZerosTest.kt
@@ -0,0 +1,15 @@
+package leetcode
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MoveZerosTest {
+    @Test
+    fun `test case 1`() {
+        val input = intArrayOf(0,1,0,3,12)
+        MoveZeros().moveZeroes(input)
+
+        val expected = intArrayOf(1,3,12,0,0)
+        assertArrayEquals(expected, input)
+    }
+}
