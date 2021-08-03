694142cc8c72306bcb904df99163405560d61813
Magdi <acm.magdi@gmail.com>
2021-08-03 22:41:00 +0200

    solution for ConstructQuadTree

---
+++ src/main/kotlin/leetcode/ConstructQuadTree.kt
@@ -0,0 +1,44 @@
+package leetcode
+
+
+class ConstructQuadTree {
+    data class Node(
+        var `val`: Boolean,
+        var isLeaf: Boolean,
+    ) {
+        var topLeft: Node? = null
+        var topRight: Node? = null
+        var bottomLeft: Node? = null
+        var bottomRight: Node? = null
+    }
+
+    fun construct(grid: Array<IntArray>): Node? {
+        if (grid.isEmpty()) {
+            return null
+        }
+        return constructQTree(grid, 0, 0, grid.size)
+    }
+
+    fun constructQTree(grid: Array<IntArray>, row: Int, col: Int, length: Int): Node {
+        if (length == 1) {
+            return Node(grid[row][col] == 1, true)
+        }
+        val mid = length / 2
+        val topLeft = constructQTree(grid, row, col, mid)
+        val topRight = constructQTree(grid, row, col + mid, mid)
+        val bottomLeft = constructQTree(grid, row + mid, col, mid)
+        val bottomRight = constructQTree(grid, row + mid, col + mid, mid)
+        if (topLeft.`val` == topRight.`val` && topRight.`val` == bottomRight.`val`
+            && bottomRight.`val` == bottomLeft.`val` && topRight.isLeaf && topLeft.isLeaf && bottomRight.isLeaf && bottomLeft.isLeaf
+        ) {
+            return Node(topLeft.`val`, true)
+        } else {
+            val node = Node(false, false)
+            node.topLeft = topLeft
+            node.topRight = topRight
+            node.bottomRight = bottomRight
+            node.bottomLeft = bottomLeft
+            return node
+        }
+    }
+}
---
+++ src/test/kotlin/leetcode/ConstructQuadTreeTest.kt
@@ -0,0 +1,75 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ConstructQuadTreeTest {
+
+    @Test
+    fun `test case 1`() {
+        val constracted = ConstructQuadTree().construct(arrayOf(intArrayOf(0, 1), intArrayOf(1, 0)))
+        val expected = ConstructQuadTree.Node(
+            `val` = true,
+            isLeaf = false
+        )
+        expected.topLeft = ConstructQuadTree.Node(false, true)
+        expected.topRight = ConstructQuadTree.Node(true, true)
+        expected.bottomLeft = ConstructQuadTree.Node(true, true)
+        expected.bottomRight = ConstructQuadTree.Node(false, true)
+        assertEquals(expected, constracted)
+    }
+
+    @Test
+    fun `test case 2`() {
+        val constracted = ConstructQuadTree().construct(arrayOf(intArrayOf(1, 1), intArrayOf(1, 1)))
+        val expected = ConstructQuadTree.Node(
+            `val` = true,
+            isLeaf = true
+        )
+        assertEquals(expected, constracted)
+    }
+
+    @Test
+    fun `test case 3`() {
+        /**
+         * [[1,1,1,1,0,0,0,0],
+         * [1,1,1,1,0,0,0,0],
+         * [1,1,1,1,1,1,1,1],
+         * [1,1,1,1,1,1,1,1]
+         * ,[1,1,1,1,0,0,0,0],
+         * [1,1,1,1,0,0,0,0],
+         * [1,1,1,1,0,0,0,0],
+         * [1,1,1,1,0,0,0,0]]
+         */
+        val constracted = ConstructQuadTree().construct(
+            arrayOf(
+                intArrayOf(1,1,1,1,0,0,0,0),
+                intArrayOf(1,1,1,1,0,0,0,0),
+                intArrayOf(1,1,1,1,1,1,1,1),
+                intArrayOf(1,1,1,1,1,1,1,1),
+                intArrayOf(1,1,1,1,0,0,0,0),
+                intArrayOf(1,1,1,1,0,0,0,0),
+                intArrayOf(1,1,1,1,0,0,0,0),
+                intArrayOf(1,1,1,1,0,0,0,0),
+            )
+        )
+        val expected = ConstructQuadTree.Node(
+            `val` = true,
+            isLeaf = false,
+        )
+        expected.topLeft = ConstructQuadTree.Node(true, true)
+        expected.topRight = ConstructQuadTree.Node(true, false)
+
+        expected.bottomLeft = ConstructQuadTree.Node(true, true)
+        expected.bottomRight = ConstructQuadTree.Node(false, true)
+
+
+        expected.topRight?.topLeft = ConstructQuadTree.Node(false, true)
+        expected.topRight?.topRight = ConstructQuadTree.Node(false, true)
+        expected.topRight?.bottomRight = ConstructQuadTree.Node(true, true)
+        expected.topRight?.bottomLeft = ConstructQuadTree.Node(true, true)
+        assertEquals(expected, constracted)
+    }
+
+}
