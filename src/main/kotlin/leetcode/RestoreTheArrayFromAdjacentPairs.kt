9efbe21c1869c4dda3c9afa2d445a0abf7bed1b9
Magdi <acm.magdi@gmail.com>
2021-08-04 17:26:35 +0200

    RestoreTheArrayFromAdjacentPairs

---
+++ src/main/kotlin/leetcode/RestoreTheArrayFromAdjacentPairs.kt
@@ -0,0 +1,43 @@
+package leetcode
+
+class RestoreTheArrayFromAdjacentPairs {
+    fun restoreArray(adjacentPairs: Array<IntArray>): IntArray {
+        if (adjacentPairs.size == 1) {
+            return adjacentPairs[0]
+        }
+        val results = IntArray(adjacentPairs.size + 1)
+        val numbersAdj = hashMapOf<Int, List<Int>>()
+        adjacentPairs.forEach { (x, y) ->
+            addAdj(numbersAdj, x, y)
+            addAdj(numbersAdj, y, x)
+        }
+        var first: Int = 0
+        numbersAdj.forEach { (num, list) ->
+            if (list.size == 1) {
+                first = num
+                return@forEach
+            }
+        }
+
+        results[0] = first
+        results[1] = numbersAdj[first]!![0]
+
+        for (index in 2 until numbersAdj.size) {
+            val parent = results[index - 2]
+            val cur = numbersAdj[results[index - 1]]!!.first { it != parent }
+            results[index] = cur
+        }
+
+        return results
+    }
+
+    private fun addAdj(
+        numbersAdj: HashMap<Int, List<Int>>,
+        x: Int,
+        y: Int
+    ) {
+        val list = numbersAdj.getOrDefault(x, emptyList()).toMutableList()
+        list.add(y)
+        numbersAdj[x] = list
+    }
+}
---
+++ src/test/kotlin/leetcode/RestoreTheArrayFromAdjacentPairsTest.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RestoreTheArrayFromAdjacentPairsTest {
+
+    @Test
+    fun `test case 1`() {
+        val res = RestoreTheArrayFromAdjacentPairs().restoreArray(
+            arrayOf(
+                intArrayOf(2,1),
+                intArrayOf(3,4),
+                intArrayOf(3,2)
+            )
+        )
+        assertArrayEquals(intArrayOf(4,3,2,1), res)
+    }
+}
