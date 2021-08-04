85f99c7da897ecf65ecfb385f85693cc6100c816
Magdi <acm.magdi@gmail.com>
2021-08-04 14:16:51 +0200

    solution for ConstructKPalindromeStrings

---
+++ src/main/kotlin/leetcode/ConstructKPalindromeStrings.kt
@@ -0,0 +1,14 @@
+package leetcode
+
+class ConstructKPalindromeStrings {
+    fun canConstruct(s: String, k: Int): Boolean {
+        if (k > s.length) return false
+        val charCount = hashMapOf<Char, Int>()
+        s.forEach { char ->
+            val cnt = charCount.getOrDefault(char, 0)
+            charCount[char] = cnt + 1
+        }
+        val oddCnt = charCount.values.count { it % 2 != 0 }
+        return oddCnt <= k
+    }
+}
---
+++ src/test/kotlin/leetcode/ConstructKPalindromeStringsTest.kt
@@ -0,0 +1,33 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ConstructKPalindromeStringsTest {
+
+    @Test
+    fun `test case 1`() {
+        assertTrue(ConstructKPalindromeStrings().canConstruct("annabelle", 2))
+    }
+
+    @Test
+    fun `test case 2`() {
+        assertFalse(ConstructKPalindromeStrings().canConstruct("leetcode", 3))
+    }
+
+    @Test
+    fun `test case 3`() {
+        assertTrue(ConstructKPalindromeStrings().canConstruct("true", 4))
+    }
+
+    @Test
+    fun `test case 4`() {
+        assertTrue(ConstructKPalindromeStrings().canConstruct("yzyzyzyzyzyzyzy", 2))
+    }
+
+    @Test
+    fun `test case 5`() {
+        assertFalse(ConstructKPalindromeStrings().canConstruct("cr", 7))
+    }
+}
