c2ab1f331351aeed9731a9f65331271c1b11ab2d
Magdi <acm.magdi@gmail.com>
2021-08-04 18:34:41 +0200

    TextJustification.kt

---
+++ src/main/kotlin/leetcode/TextJustification.kt
@@ -0,0 +1,42 @@
+package leetcode
+
+import java.lang.StringBuilder
+
+class TextJustification {
+    fun fullJustify(words: Array<String>, maxWidth: Int): List<String> {
+        val results = mutableListOf<String>()
+        var index = 0
+        while (index < words.size) {
+            val lineBuilder = StringBuilder()
+            lineBuilder.append(words[index])
+            var end = index
+            var remSpaces = maxWidth + 1
+            while (end < words.size && remSpaces - (words[end].length + 1) >= 0) {
+                remSpaces -= (words[end].length + 1)
+                end++
+            }
+            val spaceCnt = maxWidth - (index until end).map { words[it].length }.sum()
+            val wordsCnt = end - index
+            if (wordsCnt == 1) {// one word line
+                lineBuilder.append(" ".repeat(spaceCnt))
+            } else if (end == words.size) {
+                for (s in index + 1 until end) {
+                    lineBuilder.append(" ")
+                    lineBuilder.append(words[s])
+                }
+                lineBuilder.append(" ".repeat(maxWidth - lineBuilder.length))
+            } else {
+                val midSpaces = spaceCnt / (wordsCnt - 1)
+                var extraSpaces = spaceCnt % (wordsCnt - 1)
+                for (s in index + 1 until end) {
+                    val extra = if (extraSpaces-- > 0) 1 else 0
+                    lineBuilder.append(" ".repeat(midSpaces + extra))
+                    lineBuilder.append(words[s])
+                }
+            }
+            results.add(lineBuilder.toString())
+            index = end
+        }
+        return results
+    }
+}
---
+++ src/test/kotlin/leetcode/TextJustificationTest.kt
@@ -0,0 +1,51 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TextJustificationTest {
+
+    @Test
+    fun `test case 1`() {
+        val results = TextJustification().fullJustify(
+            arrayOf("This", "is", "an", "example", "of", "text", "justification."),
+            16
+        )
+        val expected = listOf(
+            "This    is    an",
+            "example  of text",
+            "justification.  "
+        )
+        assertEquals(expected, results)
+    }
+
+    @Test
+    fun `test case 3`() {
+        val results = TextJustification().fullJustify(
+            arrayOf("shall", "be"),
+            16
+        )
+        val expected = listOf(
+            "shall be        "
+        )
+        assertEquals(expected, results)
+    }
+
+    @Test
+    fun `test case 2`() {
+        val results = TextJustification().fullJustify(
+            arrayOf("Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"),
+            20
+        )
+        val expected = listOf(
+            "Science  is  what we",
+            "understand      well",
+            "enough to explain to",
+            "a  computer.  Art is",
+            "everything  else  we",
+            "do                  "
+        )
+        assertEquals(expected, results)
+    }
+}
