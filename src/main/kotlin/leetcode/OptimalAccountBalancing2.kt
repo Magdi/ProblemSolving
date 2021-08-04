f3383e50a9a02ea49a70aa8ee15c1591a82a303a
Magdi <acm.magdi@gmail.com>
2021-08-04 10:41:46 +0200

    solution for OptimalAccountBalancing dfs solution

---
+++ src/main/kotlin/leetcode/OptimalAccountBalancing2.kt
@@ -0,0 +1,41 @@
+package leetcode
+
+
+class OptimalAccountBalancing2 {
+
+    fun minTransfers(transactions: Array<IntArray>): Int {
+        val balance = hashMapOf<Int, Int>()
+        transactions.forEach { (from, to, price) ->
+            var fromBalance = balance.getOrDefault(from, 0)
+            var toBalance = balance.getOrDefault(to, 0)
+            balance[from] = fromBalance + price
+            balance[to] = toBalance - price
+        }
+
+        val balances = balance.values.toMutableList()
+        return dfs(balances, 0)
+    }
+
+    private fun dfs(balance: MutableList<Int>, ind: Int): Int {
+        if (ind == balance.size) {
+            return 0
+        }
+        if (balance[ind] == 0) return dfs(balance, ind + 1)
+        var transactions = Int.MAX_VALUE
+        for (i in ind + 1 until balance.size) {
+            if ((balance[i] * balance[ind] < 0)) {
+                balance[i] += balance[ind]
+                transactions = Math.min(transactions, 1 + dfs(balance, ind + 1))
+                balance[i] -= balance[ind]
+
+                if (balance[i] + balance[ind] == 0) {
+                    break
+                }
+            }
+        }
+
+        return transactions
+    }
+
+
+}
---
+++ src/test/kotlin/leetcode/OptimalAccountBalancing2Test.kt
@@ -0,0 +1,50 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class OptimalAccountBalancing2Test {
+
+    @Test
+    fun `minTransfers test case 1`() {
+        val transactions = arrayOf(
+            intArrayOf(0, 1, 10),
+            intArrayOf(2, 0, 5),
+        )
+        assertEquals(2, OptimalAccountBalancing2().minTransfers(transactions))
+    }
+
+    @Test
+    fun `minTransfers test case 2`() {
+        val transactions = arrayOf(
+            intArrayOf(0, 1, 10),
+            intArrayOf(1, 0, 1),
+            intArrayOf(1, 2, 5),
+            intArrayOf(2, 0, 5),
+        )
+        assertEquals(1, OptimalAccountBalancing2().minTransfers(transactions))
+    }
+
+    @Test
+    fun `minTransfers test case 3`() {
+        val transactions = arrayOf(
+            intArrayOf(0, 1, 10),
+            intArrayOf(1, 0, 1),
+            intArrayOf(1, 2, 5),
+            intArrayOf(2, 0, 4),
+        )
+        assertEquals(2, OptimalAccountBalancing2().minTransfers(transactions))
+    }
+
+    @Test
+    fun `minTransfers test case 4`() {
+        val transactions = arrayOf(
+            intArrayOf(0, 1, 5),
+            intArrayOf(2, 3, 1),
+            intArrayOf(2, 0, 1),
+            intArrayOf(4, 0, 2),
+        )
+        assertEquals(4, OptimalAccountBalancing2().minTransfers(transactions))
+    }
+}
