116158efdbb79e842373523def7fabb6324d34c2
Magdi <acm.magdi@gmail.com>
2021-08-04 19:19:34 +0200

    JumpGameVI.kt

---
+++ src/main/kotlin/leetcode/JumpGameVI.kt
@@ -0,0 +1,49 @@
+package leetcode
+
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.HashMap
+
+class JumpGameVI {
+
+    fun maxResult(nums: IntArray, k: Int): Int {
+//        val cache = hashMapOf<Int, Int>()
+//        return maximize(0, nums, k, cache)
+        return calc(nums, k)
+    }
+
+    // O (n log k*)
+    private fun calc(nums: IntArray, k: Int): Int {
+        val n = nums.size
+        val cache = IntArray(n)
+        cache[0] = nums[0]
+        val q = PriorityQueue(Comparator<Pair<Int, Int>> { p0, p1 ->
+            p1.first.compareTo(p0.first)
+        })
+        q.add(Pair(nums[0], 0))
+        for (i in 1 until n) {
+            while (q.peek().second < i - k) {
+                q.remove()
+            }
+            cache[i] = nums[i] + cache[q.peek().second]
+            q.add(Pair(cache[i], i))
+        }
+
+        return cache[n - 1]
+    }
+
+    // o ( n * k )
+    private fun maximize(index: Int, nums: IntArray, k: Int, cache: HashMap<Int, Int>): Int {
+        if (index == nums.size - 1) return nums[index]
+        if (cache[index] != null) return cache[index]!!
+        var max = Int.MIN_VALUE
+        for (i in 1..k) {
+            if (index + i < nums.size) {
+                max = Math.max(max, maximize(index + i, nums, k, cache))
+            }
+        }
+        max += nums[index]
+        cache[index] = max
+        return max
+    }
+}
---
+++ src/test/kotlin/leetcode/JumpGameVITest.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class JumpGameVITest {
+
+    @Test
+    fun `test case 1`() {
+        val result = JumpGameVI().maxResult(intArrayOf(1, -1, -2, 4, -7, 3), 2)
+        assertEquals(7, result)
+    }
+
+    @Test
+    fun `test case 2`() {
+        val result = JumpGameVI().maxResult(intArrayOf(10, -5, -2, 4, 0, 3), 3)
+        assertEquals(17, result)
+    }
+
+    @Test
+    fun `test case 3`() {
+        val result = JumpGameVI().maxResult(intArrayOf(1, -5, -20, 4, -1, 3, -6, -3), 2)
+        assertEquals(0, result)
+    }
+
+}
